6090c2461a29ec72a269033fa6d132c2093fd80e
Matin-Modarresi <modarresimatin@gmail.com>
2021-03-08 12:59:47 +0330

    primitive Project Plan

---
+++ Dot_Matirx.c
@@ -0,0 +1,24 @@
+/*
+ * Dot_Matirx.c
+ *
+ * Created: 3/8/2021 9:48:42 AM
+ * Author: Matin Modarresi
+ */
+
+#include "Header.h"
+
+void main(void)
+{
+
+while (1)
+    {
+           /*DDRB = DDRA = 0xff;
+           PORTA = 0X01;
+           PORTB = 0X07F; */ 
+           
+           showR();
+           showLaugh();
+           showM();
+           
+    }
+}
---
+++ Header.h
@@ -0,0 +1,6 @@
+#include <io.h>
+
+void showR();
+void showM();
+void showLaugh();
+void move();
